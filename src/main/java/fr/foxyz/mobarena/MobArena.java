4a12fa16b3a3ee1c6240464029fcce1d4c7243fe
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-10-26 03:23:04 +0100

    First commit

---
+++ src/main/java/fr/foxyz/mobarena/MobArena.java
@@ -0,0 +1,16 @@
+package fr.foxyz.mobarena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MobArena extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
